f807a24911706f6f72678e6d383d481c65b4fc80
Victor Chang <victorchego@hotmail.com>
2018-02-24 13:02:54 +0900

    game

--- game.js
+++ game.js
@@ -64,7 +64,7 @@ function startFuuTrap(client) {
 	resetMap();
 	randomizeFuu();
 	var str = stringMap();
-	//channel.send(str).then(message => moveFuu(message)).catch(error => channel.send("Error with Fuu game: "+error);
+	channel.send(str).then(message => moveFuu(message)).catch(error => channel.send("Error with Fuu game: "+error);
 };
 
 function stringMap() {
