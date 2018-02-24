0cfe87556e454528b2e4fc8781a3e4bb97e03785
Victor Chang <victorchego@hotmail.com>
2018-02-24 11:58:56 +0900

    game

--- game.js
+++ game.js
@@ -1,9 +1,39 @@
 var CY_CHANNEL_ID = '401660510816436224';
+var map = [[0,0,0,0,0,0,0,0,0,0],[0,0,0,0,0,0,0,0,0,0],[0,0,0,0,0,0,0,0,0,0],[0,0,0,0,0,0,0,0,0,0],[0,0,0,0,0,0,0,0,0,0],
+			[0,0,0,0,0,0,0,0,0,0],[0,0,0,0,0,0,0,0,0,0],[0,0,0,0,0,0,0,0,0,0],[0,0,0,0,0,0,0,0,0,0],[0,0,0,0,0,0,0,0,0,0]];
+/*
+   1 2 3 4 5 6 7 8 9 10 
+ 1 · · · · · · · · · ·
+ 2 · · · · · · · · · ·
+ 3 · · · · · · · · · ·
+ 4 · · · · · · · · · ·
+ 5 · · · · · · · · · ·
+ 6 · · · · · · · · · ·
+ 7 · · · · · · · · · ·
+ 8 · · · · · · · · · ·
+ 9 · · · · · · · · · ·
+10 · · · · · · · · · ·
+*/
 
 function startFuuTrap(client) {
 	var channel = client.channels.find(val => val.id = CY_CHANNEL_ID);
 	if (!channel) return;
-	channel.send('Test success');
+	var str = stringMap();
+	channel.send(str);
 };
+
+function stringMap() {
+	var str = "```\n   1 2 3 4 5 6 7 8 9 10\n";
+	for (col in map) {
+		if (col != 9) str += " " + (col+1);
+		else str += "10";
+		for (row in col) {
+			str += " ·";
+		}
+		str += "\n";
+	}
+	str += "```";
+	return str;
+}
 
 module.exports = {startFuuTrap};
