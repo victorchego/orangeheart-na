8dcf8cb89ee04f0d9f367d87d74864dceba23a42
Victor Chang <victorchego@hotmail.com>
2018-03-08 07:13:11 -0800

    radio

--- yt.js
+++ yt.js
@@ -2,11 +2,13 @@ var RADIO_CHANNEL_ID = '348328771797123073';
 var ANIME_CHANNEL_ID = '335150939029766144';
 var CY_CHANNEL_ID = '401660510816436224';
 
+var TARGET_CHANNEL_ID = ANIME_CHANNEL_ID;
+
 const ytdl = require('ytdl-core');
 const streamOptions = { seek: 0, volume: 1 };
 
-var stream = null;
-var dispatcher = null;
+//var stream = null;
+//var dispatcher = null;
 
 var queue = [];
 
@@ -19,7 +21,7 @@ const voiceCallback = (oldMember, newMember) => {
 	} 
 	else if(newUserChannel === undefined){
     // User leaves a voice channel
-		var radio_channel = client.channels.find(val => val.id == RADIO_CHANNEL_ID);
+		var radio_channel = client.channels.find(val => val.id == TARGET_CHANNEL_ID);
 		if (radio_channel == oldUserChannel) {
 			if (radio_channel.members.length == 0) {
 				dispatcher.end();
@@ -39,21 +41,25 @@ function handleMessage(msg, client) {
 		msg.channel.send('Invalid parameters.');
 		return;
 	}
-	if (args[0] == "stop") {
+	if (cmd == "stop") {
+		try {
+			radio_channel.leave();
+		}
+		catch (err) {
+			console.log(error);
+		}
+	}
+	else if (cmd == "search") {
 		
 	}
-	else if (args[0] == "search") {
-		
-	}
-	else if (args[0] == "play") {
-		msg.channel.send('Play');
-		var radio_channel = client.channels.find(val => val.id == RADIO_CHANNEL_ID);
+	else if (cmd == "play") {
+		var radio_channel = client.channels.find(val => val.id == TARGET_CHANNEL_ID);
 		if (!radio_channel) {
-			msg.channel.send('Radio channel not found.');
+			msg.channel.send('Designated voice channel not found.');
 			return;
 		}
-		if (radio_channel.members.length == 0) {
-			msg.channel.send('Please join the #radio voice channel before executing commands.');
+		if (radio_channel.members.size == 0) {
+			msg.channel.send('Please join the designated voice channel before executing commands.');
 			return;
 		}
 		if (!ytdl.validateURL(args[0])) {
@@ -61,13 +67,12 @@ function handleMessage(msg, client) {
 			return;
 		}
 		else {
-			msg.channel.send('Request');
 			radio_channel.join()
 			  .then(connection => {
-				stream = ytdl(args[1], { filter : 'audioonly' });
-				dispatcher = connection.playStream(stream, streamOptions);
-				msg.channel.send('Playing in #radio');
-				client.on('voiceStatusUpdate', voiceCallback);
+				const stream = ytdl(args[0], { filter : 'audioonly' });
+				const dispatcher = connection.playStream(stream, streamOptions);
+				msg.channel.send('Playing in the radio voice channel');
+				/*client.on('voiceStatusUpdate', voiceCallback);
 				dispatcher.once('end', reason => {
 					connection.disconnect();
 					radio_channel.leave();
@@ -79,9 +84,9 @@ function handleMessage(msg, client) {
 					radio_channel.leave();
 					client.removeListener('voiceStatusUpdate', voiceCallback);
 					return;
-				});
+				});*/
 			  })
-			  .catch(console.log('fail'));
+			  .catch(console.error);
 		}
 	}
 }
