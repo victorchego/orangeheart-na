a4fd6f1e6a2130ff2031c8a83efd030816191b4e
Victor Chang <victorchego@hotmail.com>
2018-05-30 07:51:02 -0700

    update

--- rpg.js
+++ rpg.js
@@ -404,7 +404,9 @@ function updatePropertyAll(msg, key, value) {
 
 function loadDataFromWeb(msg) {
 	request(JSON_URL, function (err, response, data) {
-		console.log(JSON_URL);
+		console.log('test '+JSON_URL);
+		console.log(JSON_LINKS);
+		console.log(msg.channel.id);
 		if (err) {
 			console.log("Error has occurred: "+error);
 			return;
