2c166f651ef3c6cdac1d59ee426c984f6c7cc30e
Victor Chang <victorchego@hotmail.com>
2019-03-16 09:34:49 -0700

    mod

---
+++ mod.js
@@ -0,0 +1,24 @@
+function msgHistoryPings(userID, ratio = 0.5) {
+	// Get messages and filter by user ID
+	channel.fetchMessages()
+	.then(messages => {return spamPings(messages, ratio);})
+	.catch(console.error);
+}
+
+function spamPings(messages, ratio = 0.5) {
+	total = 0;
+	ping = 0;
+	if (messages.length == 0) {
+		return false;
+	}
+	for (var i = 0; i < messages.length; i++) {
+		if (messages[i].mentions.everyone ||
+		messages[i].mentions.members ||
+		messages[i].mentions.roles ||
+		messages[i].mentions.users) {
+			ping++;
+		}
+		total++;
+	}
+	return ping/total > ratio;
+}
